cf5e2309b5dfcc873973b73b681066acb2e43c96
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-12-14 07:12:47 +0900

    Add branch info.

--- plugin/src/main/groovy/com/github/ksoichiro/build/info/BuildInfoPlugin.groovy
+++ plugin/src/main/groovy/com/github/ksoichiro/build/info/BuildInfoPlugin.groovy
@@ -2,7 +2,6 @@ package com.github.ksoichiro.build.info
 
 import org.ajoberstar.grgit.Commit
 import org.ajoberstar.grgit.Grgit
-import org.ajoberstar.grgit.operation.LogOp
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.bundling.Jar
@@ -22,18 +21,22 @@ class BuildInfoPlugin implements Plugin<Project> {
 
     static void mergeManifest(Project project) {
         def attributes = [:] as Map<String, ?>
+        def branch
         def commit
         def committerDate
         try {
             Grgit grgit = Grgit.open()
+            branch = grgit.branch.current.name
             Commit head = grgit.log(maxCommits: 1)[0]
             commit = head.abbreviatedId
             committerDate = head.date.format("yyyy-MM-dd HH:mm:ss Z")
         } catch (ignored) {
+            branch = "unknown"
             commit = "unknown"
             committerDate = "unknown"
         }
 
+        attributes["Git-Branch"] = branch
         attributes["Git-Commit"] = commit
         attributes["Git-Committer-Date"] = committerDate
         attributes["Build-Date"] = new Date().format("yyyy-MM-dd HH:mm:ss Z")
